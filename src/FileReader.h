6f55388515bdf99df82f912787d311b4b0d2ec35
Gregor Gullwi <gregor.gullwi@gmail.com>
2023-04-05 19:52:14 +0200

    - Added support for quoted strings in the command line parser - Removed the redundant include directories vector in the command line parser and store it in the compiler context instead - Initial #define support

--- src/FileReader.h
+++ src/FileReader.h
@@ -8,6 +8,12 @@
 
 #pragma once
 
+struct DefineDirective {
+	std::string_view name;
+	std::vector<std::string_view> args;
+	std::vector<std::string> body;
+};
+
 class FileReader {
 public:
     FileReader(const CompileContext& settings, FileTree& tree) : settings_(settings), tree_(tree) {}
@@ -23,7 +29,7 @@ public:
 
         std::string line;
         while (std::getline(stream, line)) {
-            if (line.rfind("#include", 0) == 0) {
+            if (line.find("#include", 0) == 0) {
                 std::istringstream iss(line);
                 std::string token;
                 iss >> token;
@@ -34,7 +40,7 @@ public:
                 if (token.size() < 2 || (token.front() != '"' && token.front() != '<') || (token.front() == '"' && token.back() != '"') || (token.front() == '<' && token.back() != '>')) {
                     continue;
                 }
-                std::string_view filename(token.substr(1, token.size() - 2));
+                std::string filename(token.substr(1, token.size() - 2));
                 bool found = false;
                 for (const auto& include_dir : settings_.getIncludeDirs()) {
                     std::string include_file(include_dir);
@@ -50,7 +56,11 @@ public:
                     std::cerr << "Failed to include file: " << filename << std::endl;
                     return false;
                 }
-            }
+			}
+			else if (line.find("#define", 0) == 0) {
+				std::istringstream iss(line.substr(7)); // Skip the "#define"
+				handleDefine(iss);
+			}
         }
 
         //settings_.addDependency(file);
@@ -59,6 +69,43 @@ public:
     }
 
 private:
+	void handleDefine(std::istringstream& iss) {
+		std::string name;
+		iss >> name;
+		if (name.empty()) {
+			return;
+		}
+
+		DefineDirective directive;
+		directive.name = name;
+
+		if (iss.peek() == '(') {
+			std::string arg;
+			while (std::getline(iss, arg, ',')) {
+				iss.get(); // Consume the comma or opening parenthesis
+				directive.args.push_back(arg);
+				if (iss.peek() == ')') {
+					iss.get(); // Consume the closing parenthesis
+					break;
+				}
+			}
+		}
+
+		std::string line;
+		while (std::getline(iss, line)) {
+			if (line.rfind("\\", 0) == 0) {
+				directive.body.push_back(line.substr(0, line.size() - 1));
+			}
+			else {
+				directive.body.push_back(line);
+				break;
+			}
+		}
+
+		defines_[directive.name] = directive;
+	}
+
     const CompileContext& settings_;
     FileTree& tree_;
+    std::map<std::string_view, DefineDirective> defines_;
 };
