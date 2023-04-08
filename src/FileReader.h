28a27c0c486ebf366a11806f3afbbf51bd34e959
Gregor Gullwi <gregor.gullwi@gmail.com>
2023-04-08 21:08:33 +0200

    ChatGPT: - Added support for nested #ifdef directives - Removed any white-space between # and the directive - Added built-in directive __cplusplus - Initial support for #if with operators ! and or, greater than, less than

--- src/FileReader.h
+++ src/FileReader.h
@@ -3,6 +3,8 @@
 #include <vector>
 #include <sstream>
 #include <unordered_map>
+#include <ctime>
+#include <iomanip>
 
 #include "CompileContext.h"
 #include "FileTree.h"
@@ -17,13 +19,23 @@ struct DefineDirective {
 
 class FileReader {
 public:
-    FileReader(const CompileContext& settings, FileTree& tree) : settings_(settings), tree_(tree) {}
+    FileReader(const CompileContext& settings, FileTree& tree) : settings_(settings), tree_(tree) {
+		addBuiltinDefines();
+	}
+	
+	size_t find_first_non_whitespace_after_hash(const std::string& str) {
+		size_t pos = str.find('#');
+		if (pos == std::string::npos) {
+			return pos;
+		}
+		return str.find_first_not_of(" \t", pos + 1);
+	}
 
 	bool readFile(std::string_view file) {
-		std::cout << "readFile " << file << std::endl;
-		
-		if (proccessedHeaders_.find(std::string(file.begin(), file.end()))
+		if (proccessedHeaders_.find(std::string(file)) != proccessedHeaders_.end())
 			return true;
+		
+		std::cout << "readFile " << file << std::endl;
 		
 		std::ifstream stream(file.data());
 		if (!stream.is_open()) {
@@ -34,13 +46,11 @@ public:
 		tree_.addFile(file);
 
 		std::string line;
-		bool skipping = false;
 		bool in_comment = false;
+		std::stack<bool> skipping_stack;
+		skipping_stack.push(false); // Initial state: not skipping
+
 		while (std::getline(stream, line)) {
-			if (line.size() == 0) {
-				continue;
-			}
-			
 			if (in_comment) {
 				size_t end_comment_pos = line.find("*/");
 				if (end_comment_pos != std::string::npos) {
@@ -50,19 +60,7 @@ public:
 					continue;
 				}
 			}
-
-			if (skipping) {
-				if (line.find("#endif", 0) == 0) {
-					skipping = false;
-				}
-				continue;
-			}
-
-			size_t comment_pos = line.find("//");
-			if (comment_pos != std::string::npos) {
-				continue;
-			}
-
+			
 			size_t start_comment_pos = line.find("/*");
 			if (start_comment_pos != std::string::npos) {
 				size_t end_comment_pos = line.find("*/", start_comment_pos);
@@ -72,6 +70,46 @@ public:
 					in_comment = true;
 					continue;
 				}
+			}
+			
+			if (line.size() == 0) {
+				continue;
+			}
+			
+			const bool skipping = skipping_stack.top();
+			
+			// Find the position of the '#' character
+			size_t directive_pos = line.find('#');
+			if (directive_pos != std::string::npos) {
+				// Find the position of the first non-whitespace character after the '#'
+				size_t next_pos = find_first_non_whitespace_after_hash(line);
+				if (next_pos != std::string::npos && (next_pos != directive_pos + 1)) {
+					// Remove whitespaces between '#' and the directive
+					line = line.substr(0, directive_pos + 1) + line.substr(next_pos);
+				}
+			}
+
+			if (skipping) {
+				if (line.find("#endif", 0) == 0) {
+					skipping_stack.pop();
+				} else if (line.find("#ifdef", 0) == 0 || line.find("#ifndef", 0) == 0) {
+					// Nesting, push a new skipping state
+					skipping_stack.push(true);
+				}
+				continue;
+			}
+
+			size_t comment_pos = line.find("//");
+			if (comment_pos != std::string::npos) {
+				continue;
+			}
+			
+			if (size_t filePos = line.find("__FILE__"); filePos != std::string::npos) {
+				line.replace(filePos, 8, "\"" + std::string(file) + "\"");
+			}
+			if (line.find("__LINE__") != std::string::npos) {
+				size_t line_num = std::count(std::istreambuf_iterator<char>(stream), std::istreambuf_iterator<char>(), '\n') + 1;
+					line.replace(line.find("__LINE__"), 7, std::to_string(line_num));
 			}
 
 			if (line.find("#include", 0) == 0) {
@@ -84,29 +122,43 @@ public:
 				handleDefine(iss);
 			}
 			else if (line.find("#ifdef", 0) == 0) {
-				std::string symbol(line.substr(6));
+				std::istringstream iss(line.substr(6));
+				std::string symbol;
+				iss >> symbol;
 				if (defines_.count(symbol)) {
-					skipping = false;
+					skipping_stack.push(false);
 				} else {
-					skipping = true;
+					skipping_stack.push(true);
 				}
 			}
 			else if (line.find("#ifndef", 0) == 0) {
-				std::string symbol(line.substr(7));
+				std::istringstream iss(line.substr(7));
+				std::string symbol;
+				iss >> symbol;
 				if (defines_.count(symbol)) {
-					skipping = true;
+					skipping_stack.push(true);
 				} else {
-					skipping = false;
+					skipping_stack.push(false);
 				}
 			}
+			else if (line.find("#if", 0) == 0) {
+				std::istringstream iss(line.substr(3));
+				int expression_result = !evaluate_expression(iss);
+				skipping_stack.push(expression_result == 0);
+			}
 			else if (line.find("#else", 0) == 0) {
-				skipping = !skipping;
+				skipping_stack.top() = !skipping_stack.top();
 			}
 			else if (line.find("#endif", 0) == 0) {
-				skipping = false;
+				if (!skipping_stack.empty()) {
+					skipping_stack.pop();
+				} else {
+					std::cerr << "Unmatched #endif directive" << std::endl;
+					return false;
+				}
 			}
 			else if (line.find("#pragma once", 0) == 0) {
-				proccessedHeaders_.insert(std::string(file.begin(), file.end()));
+				proccessedHeaders_.insert(std::string(file));
 			}
 			else {
 				// Handle other directives
@@ -117,6 +169,105 @@ public:
 	}
 
 private:
+	void apply_operator(std::stack<int>& values, std::stack<char>& ops) {
+		if (ops.empty() || values.size() < 1) {
+			return;
+		}
+
+		char op = ops.top();
+		ops.pop();
+
+		if (op == '!') {
+			int value = values.top();
+			values.pop();
+			values.push(!value);
+		} else if (values.size() >= 2) {
+			int right = values.top();
+			values.pop();
+			int left = values.top();
+			values.pop();
+
+			if (op == '&') {
+				values.push(left && right);
+			} else if (op == '|') {
+				values.push(left || right);
+			} else if (op == '<') {
+				values.push(left < right);
+			} else if (op == '>') {
+				values.push(left > right);
+			} else if (op == '=') {
+				values.push(left == right);
+			} else if (op == '!') {
+				values.push(left != right);
+			} else if (op == '<' && ops.top() == '=') {
+				ops.pop();
+				values.push(left <= right);
+			} else if (op == '>' && ops.top() == '=') {
+				ops.pop();
+				values.push(left >= right);
+			}
+		}
+	}
+
+	int evaluate_expression(std::istringstream& iss) {
+		std::stack<int> values;
+		std::stack<char> ops;
+		
+		auto precedence = [](char op) {
+			if (op == '&') return 1;
+			if (op == '|') return 0;
+			if (op == '>' || op == '<' || op == '=' || op == '!') return 2;
+			return -1;
+		};
+
+		while (iss) {
+			char c = iss.peek();
+			if (isdigit(c)) {
+				int value;
+				iss >> value;
+				values.push(value);
+			}
+			else if (c == '(' || c == ')' || c == '!' || c == '&' || c == '|' || c == '>' || c == '<' || c == '=' || c == '!') {
+				iss.get(); // Consume the operator
+				if (c == ')') {
+					while (!ops.empty() && ops.top() != '(') {
+						apply_operator(values, ops);
+					}
+					if (!ops.empty() && ops.top() == '(') {
+						ops.pop(); // Remove the '(' from the stack
+					}
+				}
+				else {
+					while (!ops.empty() && precedence(c) <= precedence(ops.top())) {
+						apply_operator(values, ops);
+					}
+					ops.push(c);
+				}
+			} else if (isalpha(c)) {
+				std::string keyword;
+				iss >> keyword;
+				bool negate = false;
+				if (keyword.find("defined(") == 0) {
+					keyword = keyword.substr(8);
+					keyword.erase(keyword.end()-1);
+					const bool value = defines_.count(keyword) > 0;
+					values.push(negate ? !value : value);
+				} else {
+					std::cerr << "Unknown keyword in #if directive: !" << keyword << std::endl;
+					return 0;
+				}
+			} else {
+				c = iss.get();
+			}
+		}
+
+		while (!ops.empty()) {
+			apply_operator(values, ops);
+		}
+
+		return values.top();
+	}
+
 	bool processIncludeDirective(const std::string& line, const std::string_view& current_file) {
 		std::istringstream iss(line);
 		std::string token;
@@ -154,6 +305,8 @@ private:
 			return;
 		}
 
+		std::cout << "Adding #define " << name << std::endl;
+
 		DefineDirective directive;
 		directive.name = name;
 
@@ -181,6 +334,12 @@ private:
 		}
 
 		defines_[directive.name] = directive;
+	}
+	
+	void addBuiltinDefines() {
+		// Add __cplusplus with the value corresponding to the C++ standard in use
+		// For example, C++17
+		defines_["__cplusplus"] = { "__cplusplus", {}, { "201703L" } };
 	}
 
     const CompileContext& settings_;
