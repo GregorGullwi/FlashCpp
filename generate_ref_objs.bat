f5226490f43dc532e162e554ba1d00c6a57adb43
Gregor Gullwi <gregor.gullwi@gmail.com>
2025-06-15 00:08:02 +0200

    [Gregor] Added debug info to all reference .obj files

--- generate_ref_objs.bat
+++ generate_ref_objs.bat
@@ -10,11 +10,12 @@ echo Compiling reference object files...
 call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
 
 :: Compile each source file into an object file
-cl.exe /c /Fo"%REF_DIR%\return1_ref.obj" "%REF_DIR%\return1.cpp"
-cl.exe /c /Fo"%REF_DIR%\return2func_ref.obj" "%REF_DIR%\return2func.cpp"
-cl.exe /c /Fo"%REF_DIR%\call_function_with_argument_ref.obj" "%REF_DIR%\call_function_with_argument.cpp"
-cl.exe /c /Fo"%REF_DIR%\add_function_ref.obj" "%REF_DIR%\add_function.cpp"
-cl.exe /c /Fo"%REF_DIR%\arithmetic_test_ref.obj" "%REF_DIR%\arithmetic_test.cpp"
+cl.exe /Od /DEBUG:FULL /Zi /c /Fo"%REF_DIR%\return1_ref.obj" "%REF_DIR%\return1.cpp"
+cl.exe /Od /DEBUG:FULL /Zi /c /Fo"%REF_DIR%\return2func_ref.obj" "%REF_DIR%\return2func.cpp"
+cl.exe /Od /DEBUG:FULL /Zi /c /Fo"%REF_DIR%\call_function_with_argument_ref.obj" "%REF_DIR%\call_function_with_argument.cpp"
+cl.exe /Od /DEBUG:FULL /Zi /c /Fo"%REF_DIR%\add_function_ref.obj" "%REF_DIR%\add_function.cpp"
+cl.exe /Od /DEBUG:FULL /Zi /c /Fo"%REF_DIR%\arithmetic_test_ref.obj" "%REF_DIR%\arithmetic_test.cpp"
 cl.exe /Od /DEBUG:FULL /Zi /c /Fo"test_debug_ref.obj" "test_debug.cpp"
+clang-cl.exe /Od /DEBUG:FULL /Zi /c /Fo"test_debug_clang_ref.obj" "test_debug.cpp"
 
 echo Done! 
