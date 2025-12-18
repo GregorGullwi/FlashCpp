d8ecdac5db19d59c78a95af7d60fa894bc30ac54
Gregor Gullwi <gregor.gullwi@gmail.com>
2025-12-18 08:07:52 +0100

    [Gregor] Changed the test_pointer_loop.cpp to be a bit more advanced

--- tests/test_pointer_loop.cpp
+++ tests/test_pointer_loop.cpp
@@ -1,3 +1,13 @@
+struct P
+{
+	int x{10};
+	char y = {1};
+	float z{};
+	double w{3};
+	int* p = nullptr;
+    P() = default;
+};
+
 int main() {
     int arr[2];
     arr[0] = 10;
@@ -11,6 +21,13 @@ int main() {
         sum = sum + *begin;
         begin = begin + 1;
     }
+	
+	P p[3]{};
+	P* pp = &p[0];
+	for (int i = 0; i < (sizeof(p) / sizeof(P)); ++i, ++pp)
+	{
+		pp->p = &p[i].x;
+	}
     
-    return sum;  // Expected: 30
+	return sum + *(p[0].p);  // Expected: 40
 }
