0d88aa7fb1035ffe8d4430173aaaa84423005a49
Gregor Gullwi <gregor.gullwi@gmail.com>
2026-01-17 23:02:17 +0100

    Delete test_builtin_strlen_ret5.cpp

--- tests/test_builtin_strlen_ret5.cpp
+++
@@ -1,8 +0,0 @@
-// Test __builtin_strlen builtin function
-// This is a compiler intrinsic that returns size_t (unsigned long on 64-bit)
-
-int main() {
-    const char* s = "hello";
-    unsigned long len = __builtin_strlen(s);
-    return (int)len;  // Returns 5 for "hello"
-}
