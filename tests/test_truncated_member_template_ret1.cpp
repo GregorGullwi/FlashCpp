ac639ad7ca7fd19d990554e141faec66d79bf7e5
Gregor Gullwi <gregor.gullwi@gmail.com>
2026-02-12 22:19:04 +0100

    Delete test_truncated_member_template_ret1.cpp

--- tests/test_truncated_member_template_ret1.cpp
+++
@@ -1,6 +0,0 @@
-// Test member template with truncated body
-struct Outer {
-    template <typename T>
-    struct Inner {
-        T value;
-        // Missing closing braces for both Inner and Outer
