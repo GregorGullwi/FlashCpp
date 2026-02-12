335867e19c75156f820a89ed259f51684842a416
Gregor Gullwi <gregor.gullwi@gmail.com>
2026-02-12 22:18:52 +0100

    Delete test_truncated_template_eof_ret1.cpp

--- tests/test_truncated_template_eof_ret1.cpp
+++
@@ -1,6 +0,0 @@
-// Test that truncated template class body doesn't cause infinite loop
-// This test file is intentionally malformed - missing closing brace
-template <typename T>
-struct Foo {
-    T value;
-    // Missing closing brace - should error, not hang
