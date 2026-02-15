e1cfe4b117cf9b3aca829487a5ef0983c54652ae
Gregor Gullwi <gregor.gullwi@gmail.com>
2026-02-15 22:02:31 +0100

    Delete test_lambda_specifier_order_ret42.cpp

--- tests/test_lambda_specifier_order_ret42.cpp
+++
@@ -1,8 +0,0 @@
-// Test lambda specifiers in non-standard order (C++20 lambda-specifier-seq)
-// constexpr before mutable should be accepted
-// Expected return: 42
-
-int main() {
-	auto f = [x = 10]() constexpr mutable -> int { x += 32; return x; };
-	return f();
-}
