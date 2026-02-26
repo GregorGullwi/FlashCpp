db701d9763df887633369470b37e79898575f963
Gregor <gregor.gullwi@gmail.com>
2026-02-26 11:04:36 +0100

    Modified test_heap_ret0.cpp to make sure the destructor is called

--- tests/test_heap_ret0.cpp
+++ tests/test_heap_ret0.cpp
@@ -1,11 +1,14 @@
+static int g = 0;
+
 struct Point {
-int x;
-int y;
-Point(int a, int b) : x(a), y(b) {}
+	int x;
+	int y;
+	Point(int a, int b) : x(a), y(b) {}
+	~Point() { g = x + y; }
 };
 
 int main() {
-Point* p = new Point(10, 20);
-delete p;
-return 0;
+	Point* p = new Point(10, 20);
+	delete p;
+	return g == 30 ? 0 : 1;
 }
