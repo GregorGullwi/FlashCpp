c483589ae4ee0ed74cafcdc50264f63faa35c9df
Gregor <gregor.gullwi@gmail.com>
2026-02-26 10:55:11 +0100

    Re-enabled test that was marked as _fail, but is valid C++

--- tests/test_if_decl_condition_ret42.cpp
+++ tests/test_if_decl_condition_ret42.cpp
@@ -1,7 +1,5 @@
 // Test: if statement with declaration as condition
 // Pattern: if (Type var = expr) - valid since C++98
-// Currently expected to fail: parsing succeeds but codegen doesn't support
-// variable declarations as if conditions yet (bad_any_cast).
 
 typedef int myint;
 
