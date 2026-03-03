d384c66dce480b917080f4dda7b1d278f759f27c
GregorGullwi <gregor.gullwi@gmail.com>
2026-03-03 06:40:06 +0000

    Constrain rfind search to before '$' delimiter to avoid matching inside hash suffix

--- src/AstToIr.h
+++ src/AstToIr.h
@@ -1077,7 +1077,9 @@ private:
 		auto stripHash = [](std::string_view name) -> std::string_view {
 			std::string_view base = extractBaseTemplateName(name);
 			if (!base.empty()) {
-				auto pos = name.rfind(base);
+				auto dollar_pos = name.find('$');
+				auto search_region = (dollar_pos != std::string_view::npos) ? name.substr(0, dollar_pos) : name;
+				auto pos = search_region.rfind(base);
 				if (pos != std::string_view::npos) {
 					return name.substr(0, pos + base.size());
 				}
