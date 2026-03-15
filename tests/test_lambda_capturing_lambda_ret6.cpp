cb4cc5b30b99c9dac99f375eb9b3f71dba516175
Gregor Gullwi <gregor.gullwi@gmail.com>
2026-03-15 16:26:48 +0100

    New lambda test

---
+++ tests/test_lambda_capturing_lambda_ret6.cpp
@@ -0,0 +1,11 @@
+int main() {
+    auto inner = [](auto x) { return x + 1; };
+    auto outer = [inner]() { 
+        return inner(5);  
+    };
+	auto outer_neg = [&]() { 
+        return -outer();  
+    };
+	if (outer_neg() != -6) return 1;
+    return outer() == 6 ? 0 : 1;
+}
