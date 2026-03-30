38000526f03d613d2d7529ffe1fafca4605f533a
Gregor Gullwi <gregor.gullwi@gmail.com>
2026-03-30 19:39:00 +0200

    More call sites converted

--- src/IrGenerator_Lambdas.cpp
+++ src/IrGenerator_Lambdas.cpp
@@ -32,10 +32,9 @@
 			if (type_it != getTypesByNameMap().end()) {
 				info.enclosing_struct_type_index = type_it->second->type_index_;
 			}
-		} else if (current_lambda_context_.enclosing_struct_type_index.is_valid() &&
-			current_lambda_context_.enclosing_struct_type_index.index() < getTypeInfoCount()) {
+		} else if (const TypeInfo* enclosing_type = tryGetTypeInfo(current_lambda_context_.enclosing_struct_type_index)) {
 			info.enclosing_struct_type_index = current_lambda_context_.enclosing_struct_type_index;
-			info.enclosing_struct_name = StringTable::getStringView(getTypeInfo(info.enclosing_struct_type_index).name());
+			info.enclosing_struct_name = StringTable::getStringView(enclosing_type->name());
 		}
 
 		info.lambda_body = lambda.body();
