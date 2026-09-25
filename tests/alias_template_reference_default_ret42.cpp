e5d279af839d4a7698dabb0c4362b1ded909be79
Gregor Gullwi <gregorg@axis.com>
2026-09-25 18:41:32 +0200

    Cover namespaced alias default identity

--- tests/alias_template_reference_default_ret42.cpp
+++ tests/alias_template_reference_default_ret42.cpp
@@ -7,6 +7,27 @@ struct Packet {
 	long bytes;
 };
 
+namespace north {
+struct Marker {
+	int value;
+};
+}
+
+namespace south {
+struct Marker {
+	int value;
+};
+}
+
+namespace alias_defaults {
+struct Namespaced {
+	int value;
+};
+
+template<class T = Namespaced&>
+using NamespacedReference = T;
+}
+
 template<class T = Packet&>
 using PacketReference = T;
 
@@ -26,18 +47,26 @@ enum class Status : unsigned char {
 template<class T = Status&>
 using StatusReference = T;
 
+template<class T = south::Marker&>
+using SouthMarkerReference = T;
+
 static_assert(__is_same(NativeReference<>, unsigned short&));
 static_assert(__is_same(PacketReference<>, Packet&));
 static_assert(__is_same(ConstPacketReference<>, const Packet&));
 static_assert(__is_same(PacketPointerReference<>, Packet*&));
 static_assert(__is_same(NativeRvalueReference<>, unsigned int&&));
 static_assert(__is_same(StatusReference<>, Status&));
+static_assert(__is_same(SouthMarkerReference<>, south::Marker&));
+static_assert(!__is_same(SouthMarkerReference<>, north::Marker&));
+static_assert(__is_same(alias_defaults::NamespacedReference<>, alias_defaults::Namespaced&));
 
 unsigned short native_value = 6;
 unsigned int native_rvalue = 2;
 Packet packet{4, 9};
 Packet alternate_packet{7, 5};
 Packet* packet_address = &packet;
+south::Marker south_marker{11};
+alias_defaults::Namespaced namespaced{13};
 Status status = Status::Ready;
 
 NativeReference<> native_reference = native_value;
@@ -45,6 +74,8 @@ PacketReference<> packet_reference = packet;
 ConstPacketReference<> const_packet_reference = packet;
 PacketPointerReference<> packet_pointer_reference = packet_address;
 StatusReference<> status_reference = status;
+SouthMarkerReference<> south_marker_reference = south_marker;
+alias_defaults::NamespacedReference<> namespaced_reference = namespaced;
 
 int main() {
 	NativeRvalueReference<> native_rvalue_reference = static_cast<unsigned int&&>(native_rvalue);
@@ -69,6 +100,12 @@ int main() {
 	}
 	if (packet_pointer_reference->bytes != 5) {
 		return 6;
+	}
+	if (south_marker_reference.value != 11) {
+		return 7;
+	}
+	if (namespaced_reference.value != 13) {
+		return 8;
 	}
 	return 42;
 }
